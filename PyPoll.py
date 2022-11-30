9611b3fed940b9f68368d0e11cca09ee8d6f188f
diamondcastles <sg226409@ohio.edu>
2022-11-30 13:34:02 -0500

    almost feenished

--- PyPoll.py
+++ PyPoll.py
@@ -1,9 +1,3 @@
-#The data we need to retrieve
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
 
 # Adding Dependencies
 import csv
@@ -45,11 +39,19 @@ with open(file_to_load) as election_data:
 # Begin tracking that candidate's vote count
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] +=1
-
+with open(file_to_save,"w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+    
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -63,25 +65,7 @@ with open(file_to_load) as election_data:
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
-    print(winning_candidate_summary)
+    # print(winning_candidate_summary)
 
-# print(candidate_votes)
-# Print the  total votes
-# print(total_votes)
-     # Print the file object.
-     #df = election_data.read()
-     #print(df)
 
-# msg = "meep"
-
-# print(f'{msg}')
-# make filename variable to a path to the file
-# file_to_save = os.path.join("analysis","election_analysis.txt")
-# #using open() function with "w" will allow us to write data to the file
-# open(file_to_save,"w")
-# outfile = open(file_to_save,"w")
-# #Write three counties to the file.
-# outfile.write("Counties in election\n-----------------\n")
-# outfile.write("Arapahoe\nDevner\nJefferson")
-# outfile.close()
 
